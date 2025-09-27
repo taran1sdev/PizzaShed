0b53cd91df794abb43f5e46f58257abd99a6dfbd
Tom <thomasairddev@gmail.com>
2025-09-27 12:30:58 +0100

    Created test class for LoginViewModel class

---
+++ PizzaShed/Tests/LoginViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaShed.Commands;
+using PizzaShed.Model;
+using PizzaShed.Services.Data;
+using PizzaShed.Services.Security;
+
+namespace PizzaShed.Tests
+{
+    internal class LoginViewModelTests
+    {
+        
+    }
+}
